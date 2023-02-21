Fixed MoEva using two dedicated feature processing functions. MoEva expects the inputs in the problem space (before one hot encoding or scaling)

constrained_attacks/attacks/moeva/adversarial_problem.py
@@ -92,35 +92,20 @@ class AdversarialProblem(Problem):
         # --- Prepare necessary representation of the samples
 
         # Retrieve original representation
-
+        # x_adv is in problem space representation (less features)
         x_adv = np.repeat(self.x_clean.reshape(1, -1), x.shape[0], axis=0)
         #x_adv[:, self.constraints.mutable_features] = x
 
-        obj_misclassify = self._obj_misclassify(x_adv)
-
         x_preprocessed = self.fun_distance_preprocess(x_adv)
 
-        if isinstance(x_preprocessed, tuple) and x_preprocessed[1] is not None:
-            #if it is a tuple, the first is the scaler, the second is the encoder
-            #if the scaler is not None, the input is scaled and we need to compute the distance directly
-            # but unscale to compute the constraints
+        assert x_adv.shape[1]<=x_preprocessed.shape[1], "there should be less features in the problem space than the feature space!"
+        obj_misclassify = self._obj_misclassify(x_preprocessed)
 
-            unscaled, scaler, encoder = x_preprocessed
-            obj_distance = self._obj_distance(
-            x_adv, self.x_clean_distance
-            )
-            obj_constraints = self._calculate_constraints(unscaled)
+        obj_distance = self._obj_distance(
+            x_preprocessed, self.x_clean_distance
+        )
 
-        else:
-            #otherwise, the features are not scaled and we need to scale them before computing the distances
-
-            scaled, _ = x_preprocessed
-
-            obj_distance = self._obj_distance(
-                scaled , self.x_clean_distance
-            )
-
-            obj_constraints = self._calculate_constraints(x_adv)
+        obj_constraints = self._calculate_constraints(x_adv)
 
         F = [obj_misclassify, obj_distance, obj_constraints]
 